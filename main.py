cc85fe63f29540360e3258ea999683d671e3b07f
Dylan Okyere <dylan.okyere@gmail.com>
2023-05-25 19:11:30 +0200

    getting image from wikipedia and setting it as img

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+import requests
+import wikipedia
+
+Builder.load_file('frontend.kv')
 
 class MainApp(App):
     
@@ -9,8 +14,23 @@ class MainApp(App):
 
 class FirstScreen(Screen):
     
-    def search_image(self):
-        pass 
+    def get_image_link(self):
+        query = self.manager.current_screen.ids.user_query.text
+        wikipedia_page = wikipedia.page(query, auto_suggest=False)
+        image_link = wikipedia_page.images[0]
+        return image_link
+
+    def download_image(self):
+        image_path = 'files/image.jpeg'
+        headers = {'User-Agent':'WikiImageSearch/1.0 (https://doesnotexist.com) generic-library/1.0'}
+        req = requests.get(self.get_image_link, headers=headers)
+
+        with open(image_path, 'wb') as f:
+            f.write(req.content)
+        return image_path
+
+    def set_image(self):
+            self.manager.current_screen.ids.img.source = self.download_image()
 
 class RootWidget(ScreenManager):
     pass
